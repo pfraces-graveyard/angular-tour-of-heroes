63002661196d02d72631ba9e0768cdf2c326afdf
U-USERSAD\pfracesd <pfracesd@BCN-7WYY0G2.usersad.everis.int>
2017-06-23 10:49:16 +0200

    displaying data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+ 
+import { AppComponent }  from './app.component';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
